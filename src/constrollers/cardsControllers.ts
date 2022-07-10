574e02ad1884086414e6d76b2006357eedd34463
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-07-10 03:35:35 -0300

    fix: create card statuscode

--- src/constrollers/cardsControllers.ts
+++ src/constrollers/cardsControllers.ts
@@ -1,13 +1,10 @@
 import { Request, Response } from "express";
 import { createNewCard } from "../services/createCardServices.js";
 
-
 export async function createCard(req:Request, res:Response){
     const {apiKey} = res.locals;
     const {employeeId, type} = req.body;
-
-    //console.log("body", req.body, apiKey);
     const newCard = await createNewCard(apiKey, employeeId, type);
         
-    return res.sendStatus(200);
+    return res.sendStatus(201);
 }
